59b82bfa1cdba02a73fba7d9ee5f36cd866b5cd9
Drew Price <drewprice@users.noreply.github.com>
2015-07-30 16:12:13 -0400

    Add solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  case array.size
 
+  when 1
+    array.first
+
+  when 2
+    array.join(' and ')
+
+  else
+    "#{array[0..-2].join(', ')}, and #{array.last}"
+
+  end
 end
